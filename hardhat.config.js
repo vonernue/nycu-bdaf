aa3f6ba2157dcd3f9d9eb836589e8329ef4b887d
Vonernue <seanho12345@gmail.com>
2025-03-05 20:15:11 +0800

    add zircuit mainnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,12 +7,16 @@ module.exports = {
     version: "0.8.28",
     settings: {
       evmVersion: "cancun",
-    },
+    }
   },
   networks: {
     sepolia: {
       url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`, // Use Infura or Alchemy RPC
       accounts: [`${process.env.PRIVATE_KEY}`], // Your wallet private key
+    },
+    zircuit: {
+      url: "https://zircuit-mainnet.drpc.org",
+      accounts: [`${process.env.PRIVATE_KEY}`]
     },
     zircuitGarfield: {
       url: "https://zircuit-garfield-testnet.drpc.org",
